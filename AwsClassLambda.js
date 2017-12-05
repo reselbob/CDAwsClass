1ae7641ec738eea66ed2f150337e4bbe221dcefd
Bob Reselman <robert.reselman@castingnetworks.com>
2017-12-04 17:49:22 -0800

    removed unnecessary code

--- AwsClassLambda.js
+++ AwsClassLambda.js
@@ -1,8 +1,4 @@
 exports.handler = (event, context, callback) => {
-    // TODO implement
-    const dt = new Date();
-    let msg = `Hello from AwsClassFunction() at ${dt.toString()}`;
-    msg += ` From ${event.firstName} ${event.lastName} ... ${event.status}`;
     const obj = {};
     obj.firstName = event.params.querystring.firstName.toUpperCase();
     obj.lastName = event.params.querystring.lastName.toUpperCase();
